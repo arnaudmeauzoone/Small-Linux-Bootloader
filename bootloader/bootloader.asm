debbbf7bde21ffa740755023a6a81661f21404d6
Arnaud Meauzoone <arnaud.meauzoone@gmail.com>
2018-07-06 00:34:54 +0200

    Update bootloader.asm

--- bootloader/bootloader.asm
+++ bootloader/bootloader.asm
@@ -16,25 +16,25 @@
 ;         ~                        ~
 ;         |  Protected-mode kernel |
 ; 100000  +------------------------+
-;         |  I/O memory hole	     |
-; 0A0000	+------------------------+
+;         |  I/O memory hole	   |
+; 0A0000  +------------------------+
 ;         |  Reserved for BIOS	   |	Leave as much as possible unused
 ;         ~                        ~
-;         |  Command line		       |	(Can also be below the X+10000 mark)
-; X+10000	+------------------------+
-;         |  Stack/heap		         |	For use by the kernel real-mode code.
-; X+08000	+------------------------+
-;         |  Kernel setup		       |	The kernel real-mode code.
+;         |  Command line	   |	(Can also be below the X+10000 mark)
+; X+10000 +------------------------+
+;         |  Stack/heap		   |	For use by the kernel real-mode code.
+; X+08000 +------------------------+
+;         |  Kernel setup	   |	The kernel real-mode code.
 ;         |  Kernel boot sector	   |	The kernel legacy boot sector.
 ; X       +------------------------+
-;         |  Boot loader		       |	<- Boot sector entry point 0000:7C00
-; 001000	+------------------------+
+;         |  Boot loader           |	<- Boot sector entry point 0000:7C00
+; 001000  +------------------------+
 ;         |  Reserved for MBR/BIOS |
-; 000800	+------------------------+
+; 000800  +------------------------+
 ;         |  Typically used by MBR |
-; 000600	+------------------------+
-;         |  BIOS use only	       |
-; 000000	+------------------------+
+; 000600  +------------------------+
+;         |  BIOS use only	   |
+; 000000  +------------------------+
 
 
 [BITS 16]
